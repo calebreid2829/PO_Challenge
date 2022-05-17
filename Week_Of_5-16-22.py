b80b45d8c5d04a56777626f6bff01c1373c9e173
Caleb Reid <60555714+calebreid2829@users.noreply.github.com>
2022-05-17 12:30:18 -0400

    Update Week_Of_5-16-22.py

--- Week_Of_5-16-22.py
+++ Week_Of_5-16-22.py
@@ -10,6 +10,8 @@ def SumLists(list1: list, list2: list, reversed: bool = True):
     shortest: list
     num1 = 0
     num2 = 0
+    # Assigns the shortest and longest lists in case they are different lengths
+    # Also changes them to forward order if needed
     if len(list1) > len(list2):
         if reversed:
             longest = list(list1.__reversed__())
@@ -24,6 +26,8 @@ def SumLists(list1: list, list2: list, reversed: bool = True):
         else:
             longest = list2
             shortest = list1
+    # For each list the total number is multiplied by 10 then the current number is added to it
+    # ie if the current total is 4 and the next number to add is 7: 4 * 10 = 40; 40 + 7 = 47
     for x in range(len(longest)):
         if x < len(shortest):
             num1 *= 10
@@ -31,28 +35,33 @@ def SumLists(list1: list, list2: list, reversed: bool = True):
         num2 *= 10
         num2 += longest[x]
     resultList = []
+    # Adds them together and casts to a string
     result = str(num1 + num2)
+    # Appends the current character to the resultList after being cast back to int
     for x in result:
         resultList.append(int(x))
     return resultList
 
 
 class Stack(deque):
+    # Secondary stack for tracking the minimum value
+    # Appends to the left so the smallest value is always at 0
     _min = deque()
 
     def append(self, x: _T) -> None:
+        # Appends value to min if it's the first element added
         if len(self._min) == 0:
-            # self._min = deque()
             self._min.append(x)
         elif x <= self._min[0]:
             self._min.appendleft(x)
         super().append(x)
-
+    # If the popped value equals the current minimum it is removed from min as well
+    # Because its FILO they will always be removed in the correct order
     def pop(self):
         if self._min[0] == self[len(self) - 1]:
             self._min.popleft()
         super().pop()
-
+    # Returns the current minimum
     def min(self):
         return self._min[0]
 
